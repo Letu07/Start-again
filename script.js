876627e74769d4729cffec98fb8e6ed15347d338
Tu <lean7ua@gmail.com>
2021-07-04 17:43:50 +0300

    второй вариант цикла

--- script.js
+++ script.js
@@ -10,13 +10,24 @@ const personalMovieDB =  {
     privat: false    
 };
 
-for(let i = 0; i < 3; i++) {
-const a = prompt ('Один из просмотренных фильмов?', '');
-const b = prompt ('На сколько оцените его?', '');
+// for(let i = 0; i < 3; i++) {
+// const a = prompt ('Один из просмотренных фильмов?', '');
+// const b = prompt ('На сколько оцените его?', '');
+// i++;
+// personalMovieDB.movies[a] = b;
+
+// }
+// второй вариант цикла:
+let i = 0;
+
+while(i < 2) {
+    const a = prompt ('Один из просмотренных фильмов?', '');
+    const b = prompt ('На сколько оцените его?', '');
 i++;
 personalMovieDB.movies[a] = b;
 }
 
 console.log(personalMovieDB);
+
 
 
