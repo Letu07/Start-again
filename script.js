3e943637a2829533b44e3d1b0a604d3368d1f053
Tu <lean7ua@gmail.com>
2021-08-02 12:21:37 +0300

    условия и циклы

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели?', '');
 
+
+
 const personalMovieDB =  {
     count: numberOfFilms,
     movies: {},
@@ -10,18 +12,30 @@ const personalMovieDB =  {
     privat: false    
 };
 
-let i = 0;
-
-do{
-    const a = prompt ('Один из просмотренных фильмов?', '');
-    const b = prompt ('На сколько оцените его?', '');
-i++;
-personalMovieDB.movies[a] = b;
-} while(i < 2)
-
-
-
-console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы класический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ощибка');
+}
 
 
+for (let i = 0; i < 2; i++) {
+   
+    const a = prompt ('Один из просмотренных фильмов?', ''),
+          b = prompt ('На сколько оцените его?', '');
 
+if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('done');
+} else {
+    console.log('error');
+    i--;
+}
+    
+         
+}
+    console.log(personalMovieDB);
